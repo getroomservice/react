5ceb3aa5914b63c4250b933e97d0e1367446f60c
Flaque <evanjamesconrad@gmail.com>
2019-12-31 11:34:33 -0800

    Add offline

--- src/index.ts
+++ src/index.ts
@@ -26,6 +26,12 @@ export function useRoomService<T extends KeyValueObject>(
       setState(newState);
     });
 
+    // restore from offline cache
+    // (this has an await but it's real quick)
+    const offlineState = await r.restore();
+    setState(offlineState);
+
+    // attempt to connect online
     const { state } = await r.connect();
     setState(state);
   }

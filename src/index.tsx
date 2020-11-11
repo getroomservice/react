3e5d3750c9ea3b690c5ba080028e823be119c1b3
Flaque <evanjamesconrad@gmail.com>
2020-11-10 17:19:59 -0800

    export types from browser client

--- src/index.tsx
+++ src/index.tsx
@@ -3,3 +3,10 @@ export { useMap } from './useMap';
 export { useList } from './useList';
 export { useRoom } from './useRoom';
 export { RoomServiceProvider } from './RoomServiceProvider';
+export {
+  ListClient,
+  MapClient,
+  PresenceClient,
+  RoomServiceParameters,
+  RoomClient,
+} from '@roomservice/browser';
